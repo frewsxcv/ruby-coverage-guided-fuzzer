f5ed45619239cb8ed70dd9f110c4b47e3db92c5a
Corey Farwell <coreyf@rwell.org>
2018-08-31 20:15:02 -0500

    rubocop has opinions

--- run.rb
+++ run.rb
@@ -1,7 +1,7 @@
-require "coverage"
+require 'coverage'
 Coverage.start(:all)
 # Coverage.start(branches: true)
 # Coverage.start(methods: true)
 # Coverage.start(lines: true)
-load("foo.rb", true)
+load('foo.rb', true)
 p Coverage.result

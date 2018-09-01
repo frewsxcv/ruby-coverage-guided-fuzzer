9def329a047384a6f8b8760f5c96837ab516370f
Corey Farwell <coreyf@rwell.org>
2018-08-31 20:37:36 -0500

    pass file path as cli param

--- run.rb
+++ run.rb
@@ -2,9 +2,17 @@
 
 require 'coverage'
 
+if ARGV.length != 1
+  puts 'USAGE: fuzz.rb <FILE TO FUZZ>'
+  exit
+end
+file_path = ARGV[0].freeze
+
+# TODO: ensure we don't load this file
+
 fork do
   Coverage.start(:all)
-  load('foo.rb')
+  load(file_path)
 
   fuzz(Random.new.bytes(10))
 
